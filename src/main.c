3238f6a7f03286e2e9a15c51851a4a37e0009896
Eduardo Augusto <eduaug7@gmail.com>
2020-06-03 08:54:31 -0300

    working free rtos libopencm example

--- src/main.c
+++ src/main.c
@@ -1,73 +1,26 @@
-#include <stdio.h>
-#include <string.h>
-
-#include <stm32f10x.h>
-#include <stm32f10x_it.h>
-#include <stm32f10x_conf.h>
+#include <libopencm3/stm32/rcc.h>
+#include <libopencm3/stm32/gpio.h>
 
 #include <FreeRTOS.h>
 #include <task.h>
+#include <queue.h>
 
-#define Blink_Led_Priority                          ( tskIDLE_PRIORITY )
+static void blink_led(void *arg __attribute((unused))) {
 
-GPIO_InitTypeDef GPIO_InitStructure;
-
-static void prvSetupHardware(void);
-static void prvBlinkLed(void *pvParameters);
-
-
-int main(){
-	prvSetupHardware();
-
-	xTaskCreate(prvBlinkLed, "Blink", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
-
-	vTaskStartScheduler();
-
-	for(;;);
-
-	return 0;
+    for (;;) {
+        const TickType_t xDelay = 500;
+        gpio_toggle(GPIOC, GPIO13);
+        vTaskDelay(xDelay / portTICK_RATE_MS);
+    }
 }
 
-static void prvSetupHardware(void)
-{
-	SystemInit();
-	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_4);
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
+int main(void) {
 
-	/* Configure PC.4 as Output push-pull */
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_13;
-	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
-	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_2MHz;
-	GPIO_Init(GPIOC, &GPIO_InitStructure);
+    rcc_clock_setup_in_hse_8mhz_out_72mhz();
+    rcc_periph_clock_enable(RCC_GPIOC);
+    gpio_set_mode(GPIOC, GPIO_MODE_OUTPUT_2_MHZ,
+                  GPIO_CNF_OUTPUT_PUSHPULL, GPIO13);
 
-	GPIO_ResetBits(GPIOC, GPIO_Pin_13); // Set C13 to Low level ("0")
-}
-
-static void prvBlinkLed(void *pvParameters)
-{
-	const TickType_t xDelay = 500;
-	for(;;){
-		GPIOC->ODR ^= GPIO_Pin_13;
-		vTaskDelay(xDelay / portTICK_RATE_MS);
-	}
-
-}
-
-void vApplicationStackOverflowHook( TaskHandle_t pxTask, char *pcTaskName )
-{
-	/* This function will get called if a task overflows its stack.   If the
-		parameters are corrupt then inspect pxCurrentTCB to find which was the
-		offending task. */
-    ( void ) pxTask;
-	( void ) pcTaskName;
-
-	for( ;; );
-}
-
-void assert_failed( unsigned char *pucFile, unsigned long ulLine )
-{
-	( void ) pucFile;
-	( void ) ulLine;
-
-	for( ;; );
-}
+    xTaskCreate(blink_led, "blink", configMINIMAL_STACK_SIZE, NULL, configMAX_PRIORITIES - 1, NULL);
+    vTaskStartScheduler();
+    }
