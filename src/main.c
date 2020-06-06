a23afc48c7e25efe3b3f1a42411c2002e92ffb44
Eduardo Augusto <eduaug7@gmail.com>
2020-06-06 09:15:47 -0300

    working usb cdc hello word

--- src/main.c
+++ src/main.c
@@ -1,26 +1,45 @@
-#include <libopencm3/stm32/rcc.h>
-#include <libopencm3/stm32/gpio.h>
-
+#include <FreeRTOSConfig.h>
 #include <FreeRTOS.h>
 #include <task.h>
-#include <queue.h>
 
-static void blink_led(void *arg __attribute((unused))) {
+#include <libopencm3/stm32/rcc.h>
+#include <libopencm3/stm32/gpio.h>
+#include <libopencm3/cm3/nvic.h>
+
+#include <usbcdc.h>
+
+static void
+flasher(void *arg __attribute__((unused))) {
 
     for (;;) {
-        const TickType_t xDelay = 500;
-        gpio_toggle(GPIOC, GPIO13);
-        vTaskDelay(xDelay / portTICK_RATE_MS);
+            gpio_toggle(GPIOC,GPIO13);
+            vTaskDelay(pdMS_TO_TICKS(400));
+    }
+
+}
+
+void
+usb_hello_word(void) {
+
+    usb_getc();
+    for(;;){
+        usb_printf("hello-word!\n");
     }
 }
 
-int main(void) {
+int
+main (void) {
 
-    rcc_clock_setup_in_hse_8mhz_out_72mhz();
+    rcc_clock_setup_in_hse_8mhz_out_72mhz();    // Use this for "blue pill"
     rcc_periph_clock_enable(RCC_GPIOC);
-    gpio_set_mode(GPIOC, GPIO_MODE_OUTPUT_2_MHZ,
-                  GPIO_CNF_OUTPUT_PUSHPULL, GPIO13);
+    gpio_set_mode(GPIOC,GPIO_MODE_OUTPUT_2_MHZ,GPIO_CNF_OUTPUT_PUSHPULL,GPIO13);
+    
+    usb_start(1,1);
+   
+    xTaskCreate(usb_hello_word, "hello-word", 300, NULL, configMAX_PRIORITIES-1, NULL);
+    xTaskCreate(flasher, "flash", 100, NULL, configMAX_PRIORITIES-1, NULL);
 
-    xTaskCreate(blink_led, "blink", configMINIMAL_STACK_SIZE, NULL, configMAX_PRIORITIES - 1, NULL);
     vTaskStartScheduler();
-    }
+    for (;;);
+    return 0;    
+}
